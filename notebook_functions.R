68b4c9f932521d90abdf0d2c9094af1df22dd88d
corbs117 <stevencorbett@gmail.com>
2025-05-18 22:44:29 +0100

    Update notebook_functions.R

--- notebook_functions.R
+++ notebook_functions.R
@@ -3,7 +3,7 @@ theme_update(text = element_text(size = 20))
 theme_set(theme_bw())
 
 # function to get adjusted OHLC prices for a given symbol in a data frame using dplyr
-get_ohlc <- function(symbol, start_date, end_date) {
+get_ohlc <- function(symbol, start_date= '1995-01-01', end_date= Sys.Date()) {
   data <- getSymbols(symbol, src = "yahoo", from = start_date, to = end_date, auto.assign = FALSE)
   data_df <- data.frame(date = index(data), coredata(data))
   colnames(data_df) <- tolower(sub(paste0(symbol, "\\."), "", colnames(data_df)))
